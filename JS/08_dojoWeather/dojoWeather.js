4770f65f1dca7db6687949c63d48b266256af4cf
czmud <caleb.zmuda@protonmail.com>
2022-06-29 16:37:36 -0700

    added 1 decimal place precision

--- JS/08_dojoWeather/dojoWeather.js
+++ JS/08_dojoWeather/dojoWeather.js
@@ -9,19 +9,15 @@ $(".cookies-btn").click(function(){document.querySelector(".footer-cookies").rem
 
 function convertTemp(element){
     let tempArr = document.querySelectorAll(".temp")
-    console.log(tempArr);
     
     if(element.value === "C"){
         for( let i = 0; i<tempArr.length; i++ ){
-            tempArr[i].innerHTML = Math.round((tempArr[i].innerHTML - 32) * 5/9);
+            tempArr[i].innerHTML = Math.round(((tempArr[i].innerHTML - 32) * 5/9)*10)/10;
         }
-        console.log("convert to C");
     }else if (element.value === "F"){
         for( let i = 0; i<tempArr.length; i++ ){
-            tempArr[i].innerHTML = Math.round((tempArr[i].innerHTML * 9/5)+32);
+            tempArr[i].innerHTML = Math.round(((tempArr[i].innerHTML * 9/5)+32)*10)/10;
         }
-
-        console.log("convert to F");
     }
 
 }
